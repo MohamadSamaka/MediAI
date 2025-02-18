593e2f5b2ff881238c789a989229a04dad45142c
Mohamad Samaka <mhmdkais20@gmail.com>
2025-02-18 21:30:17 +0200

    inital server

---
+++ backend/src/validators/userValidtors.js
@@ -0,0 +1,32 @@
+const Joi = require("joi");
+const JsonedResponseError = require("../errors/JsonedResponseError");
+
+const validateUserCreation = (userData) => {
+  const schema = Joi.object({
+    fName: Joi.string().min(3).max(30).required(),
+    lName: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    DateOfBirth: Joi.date().iso().required(),
+    address: Joi.string().required(),
+    roleId: Joi.string().required(),
+  }).strict();
+  const { error } = schema.validate(userData);
+  if (error) throw new JsonedResponseError(error.details[0].message, 400);
+};
+
+const validateUserUpdate = (userData) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(30).optional(),
+    email: Joi.string().email().optional(),
+    password: Joi.string().min(6).optional(),
+    roleId: Joi.string().optional(),
+  }).strict();
+  const { error } = schema.validate(userData);
+  if (error) throw new JsonedResponseError(error.details[0].message, 400);
+};
+
+module.exports = {
+  validateUserCreation,
+  validateUserUpdate,
+};
